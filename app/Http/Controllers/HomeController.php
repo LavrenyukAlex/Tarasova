0a732b5ea8a31e4947d3f120c2df84fe698baec0
LavrenyukAlex <almosk93@gmail.com>
2023-03-22 19:47:26 +0300

    4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MyController extends Controller
+class HomeController extends Controller
 {
     public function index(){
-        return view('welcome');
+        return view('home');
     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Косметологическая студия Екатерины Тарасовой</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid first">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Обо мне</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Услуги</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Отзывы</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid main-photo">
+
+    <div class="container about">
+        <p class="c-name">КОСМЕТОЛОГ <br></p>
+        <p class="name"> ЕКАТЕРИНА ТАРАСОВА <br></p>
+        <p class="a-name">Со мной Вы заново влюбитесь в свое <br> отражение в зеркале</p>
+        <button type="button" class="btn btn btn-success">Записаться на прием</button>
+        <button type="button" class="btn btn-outline-light">Прайс</button>
+    </div>
+</div>
+<div class="container-fluid second">
+    <div class="container">
+        <p class="head-about-me">ОБО МНЕ <br></p>
+        <div class="d-flex flex-row mb-2">
+            <div class="p-2"><p>Идейные соображения высшего порядка, а также сложившаяся структура организации
+                    обеспечивает широкому
+                    кругу (специалистов) участие в формировании существенных финансовых и административных условий.
+                    Задача организации, в особенности же укрепление и развитие структуры позволяет выполнять важные
+                    задания по разработке системы обучения кадров, соответствует насущным потребностям.</p></div>
+            <div class="p-2"><img src="images/2.jpg" class="photo" width="300px" height="300px" alt=""></div>
+        </div>
+    </div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+</html>
